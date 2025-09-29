3d4fb257d11f9b0e16769b9711a545b84a81f108
cat_huahua <ma.xiuyuan42@gmail.com>
2025-09-29 10:34:03 +0800

    update

--- Esp32_IO_Light/src/main.c
+++ Esp32_IO_Light/src/main.c
@@ -1,1 +1,11 @@
-void app_main(){}
+#include <stdio.h>
+#define Wait 3000
+void app_main(){
+    while (1)
+    {
+        // Infinite loop to keep the application running
+        printf("Hello, ESP32!\n");
+        vTaskDelay(Wait); // Delay for 3 seconds
+    }
+    return 0;
+}
