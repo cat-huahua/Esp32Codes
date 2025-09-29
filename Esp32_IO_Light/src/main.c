14aea240328f0efd261982bea00ce13153530b75
cat_huahua <ma.xiuyuan42@gmail.com>
2025-09-29 12:29:06 +0800

    update

--- Esp32_IO_Light/src/main.c
+++ Esp32_IO_Light/src/main.c
@@ -2,11 +2,8 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #define Wait 3000 // Wait time in milliseconds
-void app_main(){
-    while (1)
-    {
-        // Infinite loop to keep the application running
-        printf("Hello, ESP32!\n");
-        vTaskDelay(Wait); // Delay for 3 seconds
-    }
+void app_main()
+{
+    printf("Hello World!\n");
+    vTaskDelay(Wait / portTICK_PERIOD_MS); // Delay for a period of time
 }
