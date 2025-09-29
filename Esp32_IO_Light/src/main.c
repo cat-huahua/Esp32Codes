1b7d6d7b9bca7f4bb81799cc4e9827bcd399cb6d
cat_huahua <ma.xiuyuan42@gmail.com>
2025-09-29 13:19:40 +0800

    update

--- Esp32_IO_Light/src/main.c
+++ Esp32_IO_Light/src/main.c
@@ -2,10 +2,12 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
-#define LED_Pin 2
+#define LED_GPIO 2
 bool Input;
 void app_main()
 {
+    gpio_reset_pin(LED_GPIO);
+    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
     while (1)
     {
         scanf("%d", &Input);
