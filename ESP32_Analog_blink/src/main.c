59f180307972d9cc61954d7ea7c084743d5adb01
cat_huahua <ma.xiuyuan42@gmail.com>
2025-09-29 14:57:16 +0800

    update

--- ESP32_Analog_blink/src/main.c
+++ ESP32_Analog_blink/src/main.c
@@ -1,1 +1,16 @@
-void app_main() {}
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#define GPIO_NUM 2
+void app_main()
+{
+    gpio_set_direction(GPIO_NUM, GPIO_MODE_OUTPUT);
+    while (1)
+    {
+        gpio_set_level(GPIO_NUM, 1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        gpio_set_level(GPIO_NUM, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
