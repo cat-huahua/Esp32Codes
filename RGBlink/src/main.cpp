66c19c3daaf9322b0e8288073ebb107eb26ae341
cat_huahua <ma.xiuyuan42@gmail.com>
2025-11-01 16:59:16 +0800

    update

--- RGBlink/src/main.cpp
+++ RGBlink/src/main.cpp
@@ -1,18 +1,39 @@
 #include <Arduino.h>
-
+#define redPin 13
+#define greenPin 12
+#define bluePin 11
 // put function declarations here:
-int myFunction(int, int);
+int blinkLED(int pin, int delayTime);
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
+  blinkLED(redPin, 5);
+  blinkLED(greenPin, 5);
+  blinkLED(bluePin, 5);
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+int blinkLED(int pin, int delayTime)
+{
+  for (int i = 0; i < 255; i++)
+  {
+    analogWrite(pin, i);
+    delay(delayTime);
+  }
+  for (int i = 255; i >= 0; i--)
+  {
+    analogWrite(pin, i);
+    delay(delayTime);
+  }
+
+  return 0;
 }
