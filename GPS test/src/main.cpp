ccfe19fde6c35f4db28492d9d3fff73f2ebe3a28
cat_huahua <ma.xiuyuan42@gmail.com>
2025-12-06 14:06:46 +0800

    update

--- GPS test/src/main.cpp
+++ GPS test/src/main.cpp
@@ -1,18 +1,47 @@
-#include <Arduino.h>
+// Requires TinyGPSPlus library (TinyGPSPlus by Mikal Hart)
+// In Arduino IDE: Sketch -> Include Library -> Manage Libraries -> search "TinyGPSPlus"
 
-// put function declarations here:
-int myFunction(int, int);
+#include <TinyGPS++.h>
+#include <HardwareSerial.h>
+
+TinyGPSPlus gps;
+HardwareSerial SerialGPS(2); // Use UART2
+
+const int RXPin = 16; // ESP32 RX2 (connect to GPS TX)
+const int TXPin = 17; // ESP32 TX2 (connect to GPS RX)
+const uint32_t GPSBaud = 9600;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(100);
+  Serial.println("NEO-6M + ESP32 example");
+  SerialGPS.begin(GPSBaud, SERIAL_8N1, RXPin, TXPin);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // Feed GPS characters into TinyGPS++ parser
+  while (SerialGPS.available() > 0) {
+    char c = SerialGPS.read();
+    gps.encode(c);
+  }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // When a new location is available, print some useful info
+  if (gps.location.isUpdated()) {
+    Serial.print("Lat: "); Serial.println(gps.location.lat(), 6);
+    Serial.print("Lng: "); Serial.println(gps.location.lng(), 6);
+    if (gps.altitude.isValid()) {
+      Serial.print("Alt: "); Serial.print(gps.altitude.meters()); Serial.println(" m");
+    }
+    Serial.print("Sats: "); Serial.println(gps.satellites.value());
+    if (gps.time.isValid()) {
+      Serial.print("Time (UTC): ");
+      if (gps.time.hour() < 10) Serial.print('0');
+      Serial.print(gps.time.hour()); Serial.print(':');
+      if (gps.time.minute() < 10) Serial.print('0');
+      Serial.print(gps.time.minute()); Serial.print(':');
+      if (gps.time.second() < 10) Serial.print('0');
+      Serial.println(gps.time.second());
+    }
+    Serial.println();
+  }
 }
